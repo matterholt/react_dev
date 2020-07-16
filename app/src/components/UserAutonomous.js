159d21a7d5860017d1166f4858a4bdcb58938fee
MAtterholt <matterholt84@gmail.com>
2020-07-16 12:46:15 -0400

    fetching data

--- app/src/components/UserAutonomous.js
+++ app/src/components/UserAutonomous.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import UserTables from "./UserTables";
+import axios from "axios";
 
 const USER_SERVICE_URL = "https://jsonplaceholder.typicode.com/users";
 
@@ -8,7 +9,7 @@ class UserTableAutonomous extends Component {
     super(props);
     this.state = {
       isFetching: false,
-      users: [{ id: "test", name: "jane", username: "doeJ" }],
+      users: [],
     };
   }
   render() {
@@ -18,19 +19,38 @@ class UserTableAutonomous extends Component {
       </>
     );
   }
+
+  // fire off the call to get the data after the component has been mounted
+  componentDidMount() {
+    this.fetchUsers();
+    this.timer = setInterval(() => this.fetchUsers(), 5000);
+  }
+
+  // remove timer when component goes away
+  componentWillUnmount() {
+    clearInterval(this.timer);
+    this.timer = null;
+  }
+
+  async fetchUserAsync() {
+    try {
+      /* update the state of isFetching, 
+      but since the state is not all new will need to use rest parameter
+      of the user state
+      */
+      this.setState({ ...this.state, isFetching: true });
+      // obtain the data
+      const response = await axios.get(USER_SERVICE_URL);
+      // update the user state and remove the fetching
+      this.setState({ users: response.data, isFetching: false });
+    } catch (e) {
+      // catch any error and console.log them
+      // not the best way but for demo is OK
+      console.log(e);
+      this.setState({ ...this.state, isFetching: true });
+    }
+  }
+  // assign async function to variable to be called
+  fetchUsers = this.fetchUserAsync;
 }
-
-// async function fetchUsersAsync() {
-//     try {
-//         this.setState({...this.state, isFetching: true});
-//         const response = await axios.get(USER_SERVICE_URL);
-//         this.setState({users: response.data, isFetching: false});
-//     } catch (e) {
-//         console.log(e);
-//         this.setState({...this.state, isFetching: false});
-//     }
-// };
-
-// fetchUsers = this.fetchUsersAsync;
-
 export default UserTableAutonomous;

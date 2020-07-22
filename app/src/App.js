811ce974b7f5a644c225aef2a9770653f46e8d58
MAtterholt <matterholt84@gmail.com>
2020-07-22 09:39:53 -0400

    how toggle switch

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,48 @@
-import React from "react";
 import "./App.css";
+import Container from "@material-ui/core/Container";
+import React, { useState } from "react";
+import Switch from "@material-ui/core/Switch";
 
 import TableConstruct from "./components/TableConstruct";
-import Container from "@material-ui/core/Container";
 import UserAutonomous from "./components/UserAutonomous";
 import HigherOrderComps from "./components/HigherOrderComps";
 import RenderProps from "./components/RenderProps";
 import HooksReactTable from "./components/HooksReactTable";
 
 function App() {
+  const [isVisible, setIsVisible] = useState({
+    autonomous: false,
+    higherOrder: false,
+    renderProp: false,
+    hooks: false,
+  });
+
+  const handleChange = (e) => {
+    setIsVisible({ ...isVisible, [e.target.name]: e.target.checked });
+  };
+
   return (
     <div>
       <Container maxWidth="md">
         <h2>Autonomous</h2>
         <p>Fetching data at the component that requires the data</p>
-        <UserAutonomous />
+        <Switch
+          name="autonomous"
+          onChange={handleChange}
+          checked={isVisible.autonomous}
+        />
+        {isVisible.autonomous ? <UserAutonomous /> : null}
       </Container>
-
       <Container maxWidth="md">
         <h2>Higher Order Function</h2>
         <p>Fetching data at the parent component and pass down as props</p>
-        <HigherOrderComps />
+        <Switch
+          name="higherOrder"
+          onChange={handleChange}
+          checked={isVisible.higherOrder}
+        />
+
+        {isVisible.higherOrder ? <HigherOrderComps /> : null}
       </Container>
 
       <Container maxWidth="md">
@@ -29,14 +51,28 @@ function App() {
           Pretty interesting concept, similar HOA but the rendering is pass to a
           separate comp
         </p>
-        <RenderProps children={TableConstruct} />
+        <Switch
+          name="renderProp"
+          onChange={handleChange}
+          checked={isVisible.renderProp}
+        />
+
+        {isVisible.renderProp ? (
+          <RenderProps children={TableConstruct} />
+        ) : null}
       </Container>
 
       <Container maxWidth="md">
         <h2>useHooks</h2>
         <p>worked with hooks for a wail and pretty standard</p>
-        <HooksReactTable />
+        <Switch
+          name="hooks"
+          onChange={handleChange}
+          checked={isVisible.hooks}
+        />
+        {isVisible.hooks ? <HooksReactTable /> : null}
       </Container>
+
       <Container maxWidth="md">
         <h2>Fetching with Suspense</h2>
         <p>

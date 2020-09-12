a393939b04f87cb7a6d6c596e9220f87aa62c689
MAtterholt <matterholt84@gmail.com>
2020-09-11 21:23:31 -0400

    return user data feature

---
+++ reactMagicAuth/auth/pages/api/test.js
@@ -0,0 +1,3 @@
+export default async function user(req, res) {
+  res.status(200).send({ done: true });
+}
--- reactMagicAuth/auth/pages/api/user.js
+++ reactMagicAuth/auth/pages/api/user.js
@@ -1,22 +1,17 @@
 import { getSession } from "../../lib/iron";
 
-const fakeData = [
-  { nameFirst: "john", email: "john@test.com" },
-  { nameFirst: "sam", email: "sam@test.com" },
-  { nameFirst: "joe", email: "joe@test.com" },
-  {
-    nameFirst: "Matterholt",
-    email: "matterholt.dev@gmail.com",
-    employeeId: 2,
-    role: "admin",
-  },
-];
+const fakeData = {
+  nameFirst: "Matterholt",
+  email: "matterholt.dev@gmail.com",
+  employeeId: 2,
+  role: "admin",
+};
 
 export default async function user(req, res) {
   const session = await getSession(req);
   const userEmail = session.email;
   // search database and only return the user that has been logged in.
-  const userInfo = fakeData.filter((user) => userEmail === user.email);
+  const userInfo = fakeData;
 
-  res.status(200).json({ user: userInfo || null });
+  res.status(200).json({ user: fakeData || null });
 }
